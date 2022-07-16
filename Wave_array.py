ec86bb9edb7f9dd0dd87f3bf22fea1e0dfd0a145
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-07-16 15:07:06 +0530

    n=int(input()) arr=list(map(int,input().split())) if arr[0]<arr[1]:     s=0     for i in range(n-1):         if i%2==0:             if arr[i]>=arr[i+1]:                 s=1     if s==0:         print('yes')     else:         print('no') else:     s=0     for i in range(1,n):         if i%2!=0:             if arr[i]>arr[i-1]:                 s=1     if s==0:         print('yes')     else:         print('no')

--- Wave_array.py
+++ Wave_array.py
@@ -1,29 +1,22 @@
 n=int(input())
-a=list(map(int,input().split()))
-c=0
-if a[0]<a[1]:
-    if a[n-2]<a[n-1] and a[n-1]>a[0]:
-        c=1
+arr=list(map(int,input().split()))
+if arr[0]<arr[1]:
+    s=0
+    for i in range(n-1):
+        if i%2==0:
+            if arr[i]>=arr[i+1]:
+                s=1
+    if s==0:
+        print('yes')
     else:
-        c=0
-    for i in range(1,n-1,2):
-        if a[i-1]<a[i] and a[i]>a[i+1]:
-            c=1
-        else:
-            c=0
-            break
-elif a[0]>a[1]:
-    if a[n-2]>a[n-1] and a[n-1]<a[0]:
-        c=1
-    else:
-        c=0
-    for i in range(1,n-1,2):
-        if a[i-1]>a[i] and a[i]<a[i+1]:
-            c=1
-        else:
-            c=0
-            break
-if c==0:
-    print('no')
+        print('no')
 else:
-    print('yes')
+    s=0
+    for i in range(1,n):
+        if i%2!=0:
+            if arr[i]>arr[i-1]:
+                s=1
+    if s==0:
+        print('yes')
+    else:
+        print('no')

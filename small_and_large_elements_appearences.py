2afe4da7767758eb83dadaf4f79338f1b04cf7b8
Samuelsatyapaul737 <96734455+Samuelsatyapaul737@users.noreply.github.com>
2022-07-19 00:12:59 +0530

    s=list(map(str,input().split())) m=[] l=[] for i in s:     mi=min(i)     m.append(mi)     ma=max(i)     m.append(ma)     for j in i:         l.append(j) a=min(m) b=max(m) print(a,end=' ') print(l.count(a),end=' ') print(b,end=' ') print(l.count(b),end=' ')

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,7 +1,16 @@
-s=input()
-x=s.replace(" ","")
-mi=min(x)
-ma=max(x)
-c1=s.count(mi)
-c2=s.count(ma)
-print(mi,c1,ma,c2,end=' ')
+s=list(map(str,input().split()))
+m=[]
+l=[]
+for i in s:
+    mi=min(i)
+    m.append(mi)
+    ma=max(i)
+    m.append(ma)
+    for j in i:
+        l.append(j)
+a=min(m)
+b=max(m)
+print(a,end=' ')
+print(l.count(a),end=' ')
+print(b,end=' ')
+print(l.count(b),end=' ')
